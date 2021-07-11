8f8a4f46f7a846fe98e5b4e64f29691be73a56aa
awet tesfay <whitezscope@gmail.com>
2021-07-11 19:45:35 +0200

    finish the manipulation of the salary

--- assets/js/salary.js
+++ assets/js/salary.js
@@ -1,33 +1,34 @@
-let interval = (checkIfWeekends().getTime() - new Date().getTime());
+let today = new Date().getTime();
+let lastDay = lastWorkingDayOfMonth(1).getTime();
+let interval = lastDay - today;
 
 // run http request for the salary payment controller
-function runPaymentRequest() {
-    interval = (checkIfWeekends().getTime() - new Date().getTime());
-
+function salaryPaymentRequest() {
     const Http = new XMLHttpRequest();
     const url='/pay/salary/salary';
     Http.open("GET", url, true);
     Http.send(null);
+
+    interval = lastWorkingDayOfMonth(2).getTime() - new Date().getTime();
+
+    clearTimeout();
+    setTimeout(salaryPaymentRequest, interval)
 }
 
 
-function checkIfWeekends() {
+function lastWorkingDayOfMonth(month) {
+
     let today = new Date();
     // get the last day of the month
-    let lastDayOfMonth = new Date(today.getFullYear(), today.getMonth()+1, 0);
+    let lastDayOfMonth =  new Date(today.getFullYear(), today.getMonth()+month, -0);
 
     // check if it is weekends, if yes change the payment day to the friday before weekends
     if (lastDayOfMonth.getDay() === 6 ) {
-        return new Date(today.getFullYear(), today.getMonth()+1, -1);
+        return new Date(today.getFullYear(), today.getMonth()+month, -1);
     } else if (lastDayOfMonth.getDay() === 7) {
-        return new Date(today.getFullYear(), today.getMonth()+1, -2);
+        return new Date(today.getFullYear(), today.getMonth()+month, -2);
     }
-    return new Date(today.getFullYear(), today.getMonth()+1, -0);
+    return lastDayOfMonth;
 }
 
-console.log('working')
-
-setInterval(runPaymentRequest, interval);
-
-
-
+setTimeout(salaryPaymentRequest, interval);
