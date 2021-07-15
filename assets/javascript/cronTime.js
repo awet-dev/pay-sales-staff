1458a0365df1479bbdb87528cbf8523c5bf8795c
awet tesfay <whitezscope@gmail.com>
2021-07-15 19:46:40 +0200

    schedule the method successfull

--- assets/javascript/cronTime.js
+++
@@ -1,3 +0,0 @@
-let today = new Date();
-let time = new Date(today.getFullYear(), today.getMonth(), 15, 13, 5);
-
--- assets/javascript/payment.js
+++ assets/javascript/payment.js
@@ -1,11 +1,22 @@
 const CronJob = require('cron').CronJob;
+import { lastWorkingDayOfMonth, workingDayHalfOfMonth, frequencies } from './CronTime';
 
-
-// const salary = new CronJob(time, function() {
-//     console.log('I am working');
-// }, null, true, 'CET');
-//
-//
-// salary.start();
-//
-// console.log(time)
+Object.keys(frequencies).forEach(type => {
+    Object.keys(frequencies[type]).forEach(key => {
+        const job = new CronJob(frequencies[type][key], function() {
+            console.log(frequencies[type][key]);
+            if (type === 'salary' && lastWorkingDayOfMonth(1).getDate() === new Date().getDate()) {
+                const Http = new XMLHttpRequest();
+                const url="https://localhost:8000/payment/request/salary" + type;
+                Http.open("GET", url, true);
+                Http.send(null);
+            } else if (type === 'bonus' && workingDayHalfOfMonth(0).getDate() === new Date().getDate()) {
+                const Http = new XMLHttpRequest();
+                const url="https://localhost:8000/payment/request/bonus" + type;
+                Http.open("GET", url, true);
+                Http.send(null);
+            }
+        }, null, true, 'CET');
+        job.start();
+    });
+})
