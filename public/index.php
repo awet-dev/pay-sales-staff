3db3e8d0dba7a55f5ee90437e1930226b1fe08d5
Awet Tesfay <whitezscope@gmail.com>
2021-07-24 11:26:40 +0200

    add the trusting config for the heroku proxy

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Kernel;
 use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
@@ -13,6 +12,3 @@ if($trustedProxies) {
     Request::setTrustedProxies($trustedProxies, Request::HEADER_X_FORWARDED_AWS_ELB);
 }
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
